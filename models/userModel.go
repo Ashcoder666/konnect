10a04efe256762e840cf2b226ed97164b9d1429b
ashir <ashirnbasheer@gmail.com>
2024-07-23 13:57:31 +0530

    setting up structure

---
+++ models/userModel.go
@@ -0,0 +1,10 @@
+package models
+
+type User struct {
+	//		id integer [primary key]
+	//	  username varchar
+	//	  email varchar
+	//	  password varchar
+	//	  role userRole
+	//	  created_at timestamp
+}
