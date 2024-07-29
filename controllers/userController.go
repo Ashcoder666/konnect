cf4e4f98fcf72789d09049f3edd2ea23b7580767
ashir <ashirnbasheer@gmail.com>
2024-07-29 12:45:54 +0530

    get all partners endpoint is done

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,10 +1,17 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/ashcoder666/konnect/services"
 	"github.com/gin-gonic/gin"
 )
 
 func ListAllPartners(c *gin.Context) {
-	services.ListAllPartners()
+	allPartners, err := services.ListAllPartners()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": err})
+	}
+
+	c.JSON(http.StatusAccepted, gin.H{"Message": "success", "Data": allPartners})
 }
--- services/userServices.go
+++ services/userServices.go
@@ -6,14 +6,14 @@ import (
 	"github.com/ashcoder666/konnect/models"
 )
 
-func ListAllPartners() {
+func ListAllPartners() ([]models.Partner, error) {
 	var partnerInstance []models.Partner
 
 	if err := models.DB.Find(&partnerInstance).Error; err != nil {
 		fmt.Println("Error fetching partners:", err)
-		return
+		return nil, err
 	}
 
-	fmt.Println(partnerInstance)
+	return partnerInstance, nil
 
 }
